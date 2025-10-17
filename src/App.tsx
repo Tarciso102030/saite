80e20faa8aa71aae3ac2d58adaac2ca566b88b8d
Tarciso102030 <tarciso@aguiarti.com.br>
2025-10-17 15:47:57 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -230,7 +230,7 @@ function App() {
                 </li>
                 <li className="flex items-center gap-2">
                   <CheckCircle className="h-5 w-5 text-green-500" />
-                  Venda e Manutenção de BALANÇAS (Checkouts e Atendimento/Balcão)  
+                  Venda e Manutenção de Balanças (Checkouts e Atendimento/Balcão)  
                 </li>
               </ul>
             </div>
